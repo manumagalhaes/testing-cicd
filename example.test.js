da9115557cd052e696f304cb68eab29f69372e69
manumagalhaes <manu.magalhaes@ymail.com>
2021-06-09 11:40:49 +0100

    fix this

--- example.test.js
+++ example.test.js
@@ -3,7 +3,7 @@ describe("test", () => {
     [1, 2, 3],
     [2, 2, 4],
   ])(
-    "should correctly take %d and %d to make %d",
+    "should correctly take % d and % d to make % d",
     (param1, param2, expected) => {
       expect(param1 + param2).toEqual(expected);
     }
